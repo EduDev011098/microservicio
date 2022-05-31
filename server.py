4d3050ec53a4ec20a9bda2966b0ee6fbdb9859d9
bob <balllinastrinidad@gmail.com>
2022-05-30 20:34:39 -0500

    vas a arriba

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask,make_response
+from datetime import datetime
+
+app = Flask(__name__)
+
+now = datetime.now()
+
+@app.route('/dayhour',methods=['GET'])
+def day_hour():
+    year = now.year
+    month = now.month
+    day = now.day
+
+    hour = now.hour
+    minutes = now.minute
+
+    info = f'hora actual: {hour}:{minutes} fecha actual: {year}-{month}-{day}'
+
+    print(info)
+    return make_response(f'{info}',200)
+
+
+
+if __name__ == '__main__':
+    
+    app.run(debug=True, port=5000, host='0.0.0.0')
